3b6d6f1d13f1151cac7ba45c59c17e204f401e85
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-19 18:01:04 -0400

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
